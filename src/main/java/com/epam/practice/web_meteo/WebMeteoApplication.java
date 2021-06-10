d2aaf011733b3f3771a79ca5785c95b16b4fafe0
Kirill Sergun <kirillsergun@gmail.com>
2021-06-10 14:58:06 +0300

    spring boot maven proj

---
+++ src/main/java/com/epam/practice/web_meteo/WebMeteoApplication.java
@@ -0,0 +1,13 @@
+package com.epam.practice.web_meteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMeteoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMeteoApplication.class, args);
+    }
+
+}
